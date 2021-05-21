99088bb36b9798ac92adec72a7f91cbb58dfc0a6
whakapapa-HTML <uriah.heep0810@gmail.com>
2021-05-21 01:28:31 +0000

    [Start] 4-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '58aa2df4f9305ca4446e9a5a94c9bd254510e8dce2ba20cf21cd5a2b8a34656a5a4e61e32513e57bb47d87af3621b692923bb8cee1a5b9afda593bc2ede3c9e8'
